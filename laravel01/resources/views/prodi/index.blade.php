1bc3ccbd31b75ff1aefa111386777a75bbb897b7
IlhamSwd <160827594+IlhamSwd@users.noreply.github.com>
2024-06-03 14:41:21 +0700

    push

--- laravel01/resources/views/prodi/index.blade.php
+++ laravel01/resources/views/prodi/index.blade.php
@@ -8,32 +8,31 @@
       <div class="card">
         <div class="card-body">
           <h4 class="card-title">Prodi</h4>
-            <p class="card-description">
-              List Data Prodi
-            </p>
-            <a href="{{ route('prodi.create') }}" class="btn btn-rounded btn-primary">Tambah</a>
-            <div class="table-responsive">
-              <table class="table">
-                <thead>
+          <p class="card-description">
+            List Data Prodi
+          </p>
+          <a href="{{ route('prodi.create') }}" class="btn btn-rounded btn-primary">Tambah</a>
+          <div class="table-responsive">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Prodi</th>
+                  <th>Singkatan</th>
+                  <th>Nama Fakultas</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($prodi as $item)
                   <tr>
-                   <th>Prodi</th>
-                    <th>Singkatan</th>
-                    <th>Nama Fakultas</th>
+                    <td>{{ $item["nama"] }}</td>
+                    <td>{{ $item["singkatan"] }}</td>
+                    <td>{{ $item["fakultas"]["nama"] }}</td>
                   </tr>
-                </thead>
-                <tbody>
-                  @foreach ($prodi as $item)
-                    <tr>
-                      <td>{{ $item["nama"] }}</td>
-                      <td>{{ $item["singkatan"] }}</td>
-                      <td>{{ $item["fakultas"]["nama"] }}</td>
-                    </tr>
-                  @endforeach
-                </tbody>
-              </table>
-            </div>
+                @endforeach
+              </tbody>
+            </table>
           </div>
-        </div>
+        </div>  
       </div>
     </div>
   </div>
